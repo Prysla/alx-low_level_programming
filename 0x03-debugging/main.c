df296c1daf68351f4dfa51b2a760ae5c3a5c9543
Prysla <Pryslasaved@icloud.com>
2022-10-20 10:33:37 -0700

    Task 0

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is negative or positive
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
