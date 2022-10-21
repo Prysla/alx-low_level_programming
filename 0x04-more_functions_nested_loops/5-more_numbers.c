7b160cf6821bfa64af5eef4298a0baf42e70ab48
Prysla <Pryslasaved@icloud.com>
2022-10-21 04:09:27 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-		for (j >= 10)
-			_putchar(j / 10 + '0');
-		_putchar(j % 10 + '0');
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 	}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
 
