d7b850ed6f5553730d7f677aff307c90d504321d
Prysla <Pryslasaved@icloud.com>
2022-10-31 14:51:56 -0700

    Task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,6 +5,7 @@
  * *_strpbrk - searches a string for any set of bytes
  * @s: string to search
  * @accept: string containing the bytes to the bytes in accept
+ * Return: pointer to the byte in s that matches one of the bytes in accept
  * or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
@@ -24,4 +25,4 @@ char *_strpbrk(char *s, char *accept)
 	}
 
 	return (NULL);
-}	
+}
