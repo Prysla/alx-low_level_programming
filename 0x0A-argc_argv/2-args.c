472a2915de874b2306f487fea9f4877267470737
Prysla <Pryslasaved@icloud.com>
2022-11-05 04:23:23 +0000

    Task 2 || argc_argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 
-/**  * main - prints all arguments it receives
+/**
+* main - prints all arguments it receives
 * @argc: number of arguments
 * @argv: array of arguments
 *
